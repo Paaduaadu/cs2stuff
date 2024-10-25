10b6ce5bb98aeffd084e3a8ace541ad050eaf7ab
Antero Lukkonen <anterol@microsoft.com>
2024-10-25 12:16:18 +0300

    Refactored

--- eventbuffer/Program.cs
+++ eventbuffer/Program.cs
@@ -5,41 +5,49 @@ using streaming;
 
 Console.WriteLine("0.0.1");
 
-using var client = new InfluxDBClient("http://influxdb2:8086", ReadSecret("INFLUXDB_TOKEN_FILE"));
+using var client = new InfluxDBClient("http://influxdb2:8086", Extensions.ReadSecret("INFLUXDB_TOKEN_FILE"));
 var write = client.GetWriteApiAsync();
-var readOnePlayerDeath = await EventBufferFactory.GetReadEventPlayerDeath();
+Func<PointData, Task> writeMetric = metric => write.WritePointAsync(metric,  "CS2", "Wolves");
 
-var seq = EventBufferFactory
-        .ReadToEnd(readOnePlayerDeath)
-        .Where(x => !x.Equals(default));
+await EventBufferFactory
+    .ReadToEnd(await EventBufferFactory.GetReadEventPlayerDeath())
+    .Where(gameEvent => !gameEvent.Equals(default))
+    .ExtractTransformLoad(
+        transform: Extensions.ToMetric,
+        load: writeMetric);
 
-await HandleAll(write, seq);
-
-static string ReadSecret(string envVariableName) => 
-    File.ReadAllText(Environment.GetEnvironmentVariable(envVariableName)!).Trim();
-
-static async Task HandleAll(WriteApiAsync write, IAsyncEnumerable<EventPlayerDeath> seq)
-    {
-        await foreach(var x in seq){
-            await write
-                .WritePointAsync(
-                    PointData
-                        .Measurement("EventPlayerDeath")
-                        .ToTags("Attacker", x.Attacker)
-                        .ToTags("Assister", x.Assister)
-                        .ToTags("Player", x.Player)
-                        .Tag("Headshot", x.Headshot.ToString())
-                        .Tag("Weapon", x.Weapon != null ? x.Weapon.ToString() : string.Empty)
-                        .Field("Count", 1), "CS2", "Wolves");
-        }
-    }
-
-public static class InfluxDbExtensions
+public static class Extensions
 {
+    public static string ReadSecret(string envVariableName) => 
+        File.ReadAllText(Environment.GetEnvironmentVariable(envVariableName)!).Trim();
+
+    public static PointData ToMetric(this EventPlayerDeath x) => 
+        PointData
+            .Measurement("EventPlayerDeath")
+            .ToTags("Attacker", x.Attacker)
+            .ToTags("Assister", x.Assister)
+            .ToTags("Player", x.Player)
+            .Tag("Headshot", x.Headshot.ToString())
+            .Tag("Weapon", x.Weapon != null ? x.Weapon.ToString() : string.Empty)
+            .Field("Count", 1);
+
     public static PointData ToTags(this PointData pd, string label, PlayerController? pc) =>
         pc == null
             ? pd
             : pd
                 .Tag(label, pc.PlayerName)
                 .Tag($"{label}.IsBot", pc.IsBot.ToString());
+
+    
+    public static async Task ExtractTransformLoad<TIn, TOut>(
+        this IAsyncEnumerable<TIn> extract, 
+        Func<TIn, TOut> transform,
+        Func<TOut, Task> load)
+    {
+        await foreach(var x in extract)
+        {
+            await load(transform(x));
+        }
+    }
 }
+

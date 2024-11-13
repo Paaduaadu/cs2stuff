a3037daa7d333b181e3d37cf3d6f9e2e75dc7eaa
Antero Lukkonen <anterol@microsoft.com>
2024-11-13 20:12:54 +0200

    refactored and cleanup

--- publishmetrics/Program.cs
+++ publishmetrics/Program.cs
@@ -6,18 +6,17 @@ var influxHost = args.ElementAtOrDefault(0) ?? "http://influxdb2:8086";
 var queryFilesPath = args.ElementAtOrDefault(1) ?? "/App/Queries";
 var resultFilesPath = args.ElementAtOrDefault(2) ?? "/App/Results";
 
-var writersCache = new Dictionary<string, StreamWriter>();
-var getWriter = FunctionalExtensions.Memoize(path =>
-    new StreamWriter(new FileStream(path, FileMode.Create)), writersCache);
-
+var streamCache = new Dictionary<string, Stream>();
+var getTargetStream = FunctionalExtensions.Memoize(path =>
+    new FileStream(path, FileMode.Create), streamCache);
 
 await Task.WhenAll(
     Process<PlayersDataRecord>([Path.Combine(queryFilesPath, "players_data.flux")], x => x.Last()),
     Process<PlayerStatsDataRecord>([Path.Combine(queryFilesPath, "player_stats_data.flux")], x => x.Single()));
 
-writersCache.Values.ToList().ForEach(w => w.Close());
+streamCache.Values.ToList().ForEach(w => w.Close());
 
-IAsyncEnumerable<Task> EnumerateResults<T>(IEnumerable<string> queryFiles, Func<IEnumerable<T>, T> aggregate) where T : BaseRecord, new() =>
+IAsyncEnumerable<Task> EnumerateResults<T>(IEnumerable<string> queryFiles, Func<IEnumerable<T>, T> aggregateDuplicates) where T : BaseRecord, new() =>
     queryFiles
         .Select(file =>
             new
@@ -28,19 +27,17 @@ IAsyncEnumerable<Task> EnumerateResults<T>(IEnumerable<string> queryFiles, Func<
             }
         )
         .ToAsyncEnumerable()
-        .Select(query =>
+        .SelectAwait(async querySpec =>
             (
-                query.Name,
-                Results: query.Read(query.Query),
-                Writer: getWriter(Path.Combine(resultFilesPath, query.Name + ".json"))))
-        .Select(async records =>
-            await JsonSerializer.SerializeAsync(
-                records.Writer.BaseStream,
-                await records
-                    .Results
+                Result: await querySpec
+                    .Read(querySpec.Query)
                     .Where(rec => !string.IsNullOrEmpty(rec.SteamID))
-                    .GroupBy(x => x.SteamID)
-                    .ToDictionaryAsync(rec => rec.Key!, rec => aggregate(rec.ToEnumerable()))));
+                    .ToDictionaryAsync(x => x.SteamID!, aggregateDuplicates),
+                TargetStream: getTargetStream(Path.Combine(resultFilesPath, querySpec.Name + ".json"))))
+        .Select(resultsSpec =>
+            JsonSerializer.SerializeAsync(
+                resultsSpec.TargetStream,
+                resultsSpec.Result));
 
 static Func<string, IAsyncEnumerable<T>> GetRead<T>(string influxHost) where T : BaseRecord, new()
 {

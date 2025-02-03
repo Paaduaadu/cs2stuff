b070f197906e3687ae72cd121f68cd936d2da7e0
Antero Lukkonen <anterol@microsoft.com>
2025-02-03 12:28:56 +0200

    updated plugins

--- pre.sh
+++ pre.sh
@@ -1,31 +1,33 @@
 
-cssZip = "counterstrikesharp-with-runtime-build-287-linux-6cf124b.zip"
-cssDownloadPath = "v287/$cssZip"
-cssDownloadUrl = "https://github.com/roflmuffin/CounterStrikeSharp/releases/download/$cssDownloadPath"
+cssZip="counterstrikesharp-with-runtime-build-305-linux-e99d27c.zip"
+cssDownloadPath="v305/$cssZip"
+cssDownloadUrl="https://github.com/roflmuffin/CounterStrikeSharp/releases/download/$cssDownloadPath"
+metamodPackage="mmsource-2.0.0-git1319-linux.tar.gz"
 
 cssPluginHome="/home/steam/exportevents"
 csgoDir="/home/steam/cs2-dedicated/game/csgo"
 addonsDir="$csgoDir/addons"
 file="$addonsDir/counterstrikesharp/plugins/exporteventsplugin"
+
 if [ ! -e "$file" ]; then
     mkdir -p $addonsDir
     
-    wget https://mms.alliedmods.net/mmsdrop/2.0/mmsource-2.0.0-git1314-linux.tar.gz
-    tar -xvzf mmsource-2.0.0-git1314-linux.tar.gz
+    wget https://mms.alliedmods.net/mmsdrop/2.0/$metamodPackage
+    tar -xvzf $metamodPackage
     
     mv addons $csgoDir
-    rm -rf mmsource-2.0.0-git1314-linux.tar.gz
+    rm -rf $metamodPackage
 
     perl -i -pe 's/Game\s+csgo\r?\n/Game        csgo\/addons\/metamod
                 Game    csgo
     /g' game/csgo/gameinfo.gi
 
     
-    wget cssDownloadUrl
-    unzip cssZip
+    wget $cssDownloadUrl
+    unzip $cssZip
     cp -R addons/* $addonsDir
     rm -rf addons
-    rm -rf cssZip
+    rm -rf $cssZip
 else
     echo "pre.sh has been executed already once."
 fi
